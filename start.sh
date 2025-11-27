b3b5d6f068cd059b61d77fd48d0496930481f2e6
Isfandiyor0112-star <imamalievisfandier@gmail.com>
2025-11-27 16:25:11 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ if [ -z "$YT_URL" ] || [ -z "$TG_RTMP" ] || [ -z "$TG_KEY" ]; then
   exit 1
 fi
 
-# Получаем прямую HLS ссылку на YouTube
+# Функция получения прямой HLS ссылки для YouTube
 get_hls() {
   yt-dlp -g "$YT_URL" 2>/dev/null | tail -n 1
 }
@@ -21,10 +21,10 @@ while true; do
 
   echo "Relay: $HLS_URL -> rtmp://$TG_RTMP/$TG_KEY"
 
-  # ffmpeg гонит поток в Telegram
+  # ffmpeg гонит поток в Telegram с преобразованием в вертикальный формат 1080x1920
   ffmpeg -hide_banner -loglevel warning \
     -re -i "$HLS_URL" \
-    -vf "fps=${FPS}" \
+    -vf "scale=1080:-2, pad=1080:1920:(ow-iw)/2:(oh-ih)/2, fps=${FPS}" \
     -c:v libx264 -preset "${X264_PRESET}" -b:v "${VIDEO_BITRATE}" -maxrate "${VIDEO_BITRATE}" -bufsize "$((2*${VIDEO_BITRATE%k}))k" \
     -c:a aac -b:a "${AUDIO_BITRATE}" -ar 44100 \
     -f flv "rtmp://$TG_RTMP/$TG_KEY"
