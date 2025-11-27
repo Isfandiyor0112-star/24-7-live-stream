c528f0f44076e9a6fe2ba67403ba1301e31461e2
Isfandiyor0112-star <imamalievisfandier@gmail.com>
2025-11-27 17:16:40 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,17 +9,18 @@ fi
 # Запускаем ffmpeg в фоне
 (
 while true; do
-HLS_URL="$(yt-dlp -f "bestvideo+bestaudio" --merge-output-format mp4 -g "https://www.youtube.com/watch?v=4dUr6L-dKgo" 2>/dev/null | head -n 1)"
-  if [ -z "$HLS_URL" ]; then
-    echo "Failed to get HLS URL. Retry in 10s..."
+  # Получаем прямую ссылку на обычное видео
+  VIDEO_URL="$(yt-dlp -f 'bestvideo+bestaudio' --merge-output-format mp4 -g "$YT_URL" 2>/dev/null | head -n 1)"
+  if [ -z "$VIDEO_URL" ]; then
+    echo "Failed to get video URL. Retry in 10s..."
     sleep 10
     continue
   fi
 
-  echo "Relay: $HLS_URL -> rtmp://$TG_RTMP/$TG_KEY"
+  echo "Relay: $VIDEO_URL -> rtmp://$TG_RTMP/$TG_KEY"
 
   ffmpeg -hide_banner -loglevel warning \
-    -re -i "$HLS_URL" \
+    -re -i "$VIDEO_URL" \
     -vf "scale=1080:-2, pad=1080:1920:(ow-iw)/2:(oh-ih)/2, fps=${FPS}" \
     -c:v libx264 -preset "${X264_PRESET}" -b:v "${VIDEO_BITRATE}" -maxrate "${VIDEO_BITRATE}" -bufsize "$((2*${VIDEO_BITRATE%k}))k" \
     -c:a aac -b:a "${AUDIO_BITRATE}" -ar 44100 \
