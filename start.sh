64fb5223697db7acd9b003540efcf131d1c0d8c7
Isfandiyor0112-star <imamalievisfandier@gmail.com>
2025-12-11 21:23:02 +0500

    Change streaming source to Google Drive link

--- start.sh
+++ start.sh
@@ -8,10 +8,10 @@ fi
 
 (
   while true; do
-    echo "Streaming local mp4 file -> rtmp://$TG_RTMP/$TG_KEY"
+    echo "Streaming mp4 from Google Drive -> rtmp://$TG_RTMP/$TG_KEY"
 
     ffmpeg -hide_banner -loglevel warning \
-      -re -stream_loop -1 -i music.mp4 \
+      -re -stream_loop -1 -i "https://drive.google.com/uc?id=1Bj5c2QXScUQQRI469euvST5PFfaUu_e3" \
       -vf "scale=1080:-2, pad=1080:1920:(ow-iw)/2:(oh-ih)/2, fps=${FPS}" \
       -c:v libx264 -preset "${X264_PRESET}" -b:v "${VIDEO_BITRATE}" -maxrate "${VIDEO_BITRATE}" -bufsize "$((2*${VIDEO_BITRATE%k}))k" \
       -c:a aac -b:a "${AUDIO_BITRATE}" -ar 44100 \
@@ -22,5 +22,4 @@ fi
   done
 ) &
 
-# Health-check для Render
 python3 -m http.server 8080
