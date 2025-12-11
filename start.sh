7f9e5f22e622578e4adad519bdec7356241162bb
Isfandiyor0112-star <imamalievisfandier@gmail.com>
2025-12-11 20:50:43 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 set -e
 
-if [ -z "$YT_URL" ] || [ -z "$TG_RTMP" ] || [ -z "$TG_KEY" ]; then
-  echo "Missing env: YT_URL, TG_RTMP, TG_KEY"
+if [ -z "$TG_RTMP" ] || [ -z "$TG_KEY" ]; then
+  echo "Missing env: TG_RTMP, TG_KEY"
   exit 1
 fi
 
-# Запускаем ffmpeg в фоне
 (
   while true; do
-    echo "Downloading video from $YT_URL..."
-    yt-dlp -f "bestvideo+bestaudio" --merge-output-format mp4 -o video.mp4 "$YT_URL"
-    
-    if [ ! -f video.mp4 ]; then
-      echo "Download failed. Retry in 10s..."
-      sleep 10
-      continue
-    fi
-
-    echo "Streaming video.mp4 -> rtmp://$TG_RTMP/$TG_KEY"
+    echo "Streaming black screen + music -> rtmp://$TG_RTMP/$TG_KEY"
 
     ffmpeg -hide_banner -loglevel warning \
-      -re -i video.mp4 \
-      -vf "scale=1080:-2, pad=1080:1920:(ow-iw)/2:(oh-ih)/2, fps=${FPS}" \
+      -f lavfi -i "color=c=black:s=1080x1920:r=${FPS}" \
+      -i music.mp3 \
+      -shortest \
       -c:v libx264 -preset "${X264_PRESET}" -b:v "${VIDEO_BITRATE}" -maxrate "${VIDEO_BITRATE}" -bufsize "$((2*${VIDEO_BITRATE%k}))k" \
       -c:a aac -b:a "${AUDIO_BITRATE}" -ar 44100 \
       -f flv "rtmp://$TG_RTMP/$TG_KEY"
@@ -32,5 +23,4 @@ fi
   done
 ) &
 
-# Минимальный HTTP-сервер для Render health check
 python3 -m http.server 8080
